2f416cd39995160f53b203f77f1f215e840492c1
DudaAl3ix0 <116288612+DudaAl3ix0@users.noreply.github.com>
2023-02-16 15:51:55 -0300

    Criação da class Funcionario
    Criação da class funcionario com seus atributos privados(possuindo todos get e set) e também do cálculo de bonificação.

---
+++ TestaBonificacao.java
@@ -0,0 +1,8 @@
+public class TestaBonificacao{
+    public static void main(String[] args) {
+        Funcionario funcionario1 = new Funcionario();
+        funcionario1.setSalario(1200);
+        System.out.println(funcionario1.getSalario());
+        System.out.println(funcionario1.getBonificacao());
+    }
+}
