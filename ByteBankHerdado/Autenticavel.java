b564281cc64d7d9aa4041df75924dd76f9172ba2
DudaAl3ix0 <116288612+DudaAl3ix0@users.noreply.github.com>
2023-02-25 20:59:42 -0300

    Não precisa de abstract.
    Se já é interface, já se torna abstract.

--- ByteBankHerdado/Autenticavel.java
+++ ByteBankHerdado/Autenticavel.java
@@ -1,7 +1,7 @@
 package ByteBankHerdado;
 //Quem assina o contrato com a interface deve implementar o método.
 //Só define regras que devem ser implementadas por quem assina o contrato.
-public abstract interface Autenticavel {
+public interface Autenticavel {
 
     public abstract void setSenha(int senha);
 
